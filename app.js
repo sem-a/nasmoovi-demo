d6b0919dc7939f17260627cd85f114bc840e384f
sem-a <aleksey.i99@mail.ru>
2024-06-13 22:13:51 +0300

    new build with static page

--- app.js
+++ app.js
@@ -34,5 +34,8 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
